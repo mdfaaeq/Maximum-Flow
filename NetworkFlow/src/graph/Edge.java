6ba0f6eb4f6e46e749e961416fddb03e34a195ed
Faaeq Fazal <faaeqavl@gmail.com>
2025-04-23 17:04:19 +0530

    Made a few changes with the Edge and Updated the Graph.java

--- NetworkFlow/src/graph/Edge.java
+++ NetworkFlow/src/graph/Edge.java
@@ -3,32 +3,33 @@ package graph;
 // This class shows the correction from one node to another with a flow limit.
 public class Edge {
     /*
-    * from: The source node ID.
-    * to: The target code ID.
-    * capacity: The maximum amount of flow allowed through this edge.
-    * flow: The current flow through the edge which starts at 0.
-    * residual: This is used to reverse the flow if needed in Ford-Fulkerson (undo or adjust flows).
-    * */
-    int from, to, capacity, flow;
-    Edge residual;
+     * fromNode: Starting point of the edge
+     * toNode: End point of the edge
+     * maxFlow: Maximum flow allowed through this edge
+     * currentFlow: Flow currently passing through this edge
+     * reverseEdge: The opposite edge used to undo/adjust flow
+     */
+    int fromNode, toNode, maxFlow, currentFlow;
+    Edge reverseEdge;
 
-    public Edge(int from, int to, int capacity) {
-        this.from = from;
-        this.to = to;
-        this.capacity = capacity;
-        this.flow = 0;
+    public Edge(int fromNode, int toNode, int maxFlow) {
+        this.fromNode = fromNode;
+        this.toNode = toNode;
+        this.maxFlow = maxFlow;
+        this.currentFlow = 0;
     }
 
-    public int remainCapacity() {
-        return capacity - flow;
+    public int getRemainingFlow() {
+        return maxFlow - currentFlow;
     }
 
-    public void augment(int bottleNeck) {
-        flow += bottleNeck;
-        residual.flow -= bottleNeck;
+    public void addFlow(int flowToAdd) {
+        currentFlow += flowToAdd;
+        reverseEdge.currentFlow -= flowToAdd;
     }
 
+    @Override
     public String toString() {
-        return String.format("Edge %d -> %d | flow = %d / %d", from, to, flow, capacity);
+        return String.format("Edge %d -> %d | flow = %d / %d", fromNode, toNode, currentFlow, maxFlow);
     }
 }
--- NetworkFlow/src/graph/Graph.java
+++ NetworkFlow/src/graph/Graph.java
@@ -1,4 +1,31 @@
 package graph;
 
+import java.util.*;
+
 public class Graph {
+    int numberOfNodes;
+    List<Edge>[] edgesFromNode;
+
+    @SuppressWarnings("unchecked")
+    public Graph(int numberOfNodes) {
+        this.numberOfNodes = numberOfNodes;
+        edgesFromNode = new ArrayList[numberOfNodes];
+        for (int i = 0; i < numberOfNodes; i++) {
+            edgesFromNode[i] = new ArrayList<>();
+        }
+    }
+
+    public void addEdge(int fromNode, int toNode, int maxFlow) {
+        Edge forwardEdge = new Edge(fromNode, toNode, maxFlow);
+        Edge reverseEdge = new Edge(toNode, fromNode, 0);
+        forwardEdge.reverseEdge = reverseEdge;
+        reverseEdge.reverseEdge = forwardEdge;
+        edgesFromNode[fromNode].add(forwardEdge);
+        edgesFromNode[toNode].add(reverseEdge);
+    }
+
+    public List<Edge>[] getEdgesFromNode() {
+        return edgesFromNode;
+    }
 }
+
