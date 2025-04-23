3a848d5b1a05669314a984645c59dba8f92f774d
Faaeq Fazal <faaeqavl@gmail.com>
2025-04-23 16:45:48 +0530

    Almost completed the Edge.java

--- NetworkFlow/src/graph/Edge.java
+++ NetworkFlow/src/graph/Edge.java
@@ -1,4 +1,27 @@
 package graph;
 
 public class Edge {
+
+    int from, to, capacity, flow;
+    Edge residual;
+
+    public Edge(int from, int to, int capacity) {
+        this.from = from;
+        this.to = to;
+        this.capacity = capacity;
+        this.flow = 0;
+    }
+
+    public int remainCapacity() {
+        return capacity - flow;
+    }
+
+    public void augment(int bottleNeck) {
+        flow += bottleNeck;
+        residual.flow -= bottleNeck;
+    }
+
+    public String toString() {
+        return String.format("Edge %d -> %d | flow = %d / %d", from, to, flow, capacity);
+    }
 }
