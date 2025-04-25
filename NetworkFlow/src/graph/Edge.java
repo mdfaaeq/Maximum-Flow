ec1db162db298158c5659e817f9c6370671a1103
Faaeq Fazal <faaeqavl@gmail.com>
2025-04-25 21:27:04 +0530

    Update Edge.java

--- NetworkFlow/src/graph/Edge.java
+++ NetworkFlow/src/graph/Edge.java
@@ -1,25 +1,26 @@
 package graph;
 
-// This class shows the correction from one node to another with a flow limit.
 public class Edge {
-    /*
-     * sourceNode: Starting point of the edge
-     * destinationNode: End point of the edge
-     * maxCapacity: Maximum flow allowed through this edge
-     * currentFlow: Flow currently passing through this edge
-     * backEdge: The opposite edge used to undo/adjust flow
-     */
     private int sourceNode;
     private int destinationNode;
-    private int maxCapacity;
+    private int capacity;
     private int currentFlow;
-    private Edge backEdge;
+    private Edge reverseEdge;
 
-    public Edge(int sourceNode, int destinationNode, int maxCapacity) {
+    public Edge(int sourceNode, int destinationNode, int capacity) {
         this.sourceNode = sourceNode;
         this.destinationNode = destinationNode;
-        this.maxCapacity = maxCapacity;
+        this.capacity = capacity;
         this.currentFlow = 0;
+    }
+
+    public int remainingCapacity() {
+        return capacity - currentFlow;
+    }
+
+    public void augment(int minResidualCapacity) {
+        this.currentFlow += minResidualCapacity;
+        this.reverseEdge.currentFlow -= minResidualCapacity;
     }
 
     public int getSourceNode() {
@@ -30,22 +31,24 @@ public class Edge {
         return destinationNode;
     }
 
-    public int remainingCapacity() {
-        return maxCapacity - currentFlow;
+    public int getCapacity() {
+        return capacity;
     }
 
-    public void augment(int flowAmount) {
-        currentFlow += flowAmount;
-        backEdge.currentFlow -= flowAmount;
+    public int getCurrentFlow() {
+        return currentFlow;
     }
 
-    public void setBackEdge(Edge backEdge) {
-        this.backEdge = backEdge;
+    public Edge getReverseEdge() {
+        return reverseEdge;
+    }
+
+    public void setReverseEdge(Edge reverseEdge) {
+        this.reverseEdge = reverseEdge;
     }
 
     @Override
     public String toString() {
-        return String.format("Edge %d -> %d | flow = %d / %d", sourceNode, destinationNode, currentFlow, maxCapacity);
+        return String.format("Edge %d -> %d | flow = %d / %d", sourceNode, destinationNode, currentFlow, capacity);
     }
-
 }
