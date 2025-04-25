a93effe3c67ddb108ae88d483071e2be407da0bc
Faaeq Fazal <faaeqavl@gmail.com>
2025-04-25 21:48:53 +0530

    Update Graph.java

--- NetworkFlow/src/graph/Graph.java
+++ NetworkFlow/src/graph/Graph.java
@@ -4,32 +4,31 @@ import java.util.*;
 
 public class Graph {
     private int numberOfNodes;
-    private List<Edge>[] neighbors;
+    private List<Edge>[] adjacencyList;
 
     @SuppressWarnings("unchecked")
     public Graph(int numberOfNodes) {
         this.numberOfNodes = numberOfNodes;
-        neighbors = new ArrayList[numberOfNodes];
-        for (int i = 0; i < numberOfNodes; i++)
-            neighbors[i] = new ArrayList<>();
+        adjacencyList = new ArrayList[numberOfNodes];
+        for (int i = 0; i < numberOfNodes; i++) {
+            adjacencyList[i] = new ArrayList<>();
+        }
     }
 
     public void addEdge(int sourceNode, int destinationNode, int capacity) {
         Edge forwardEdge = new Edge(sourceNode, destinationNode, capacity);
         Edge backwardEdge = new Edge(destinationNode, sourceNode, 0);
-
-        forwardEdge.setBackEdge(backwardEdge);
-        backwardEdge.setBackEdge(forwardEdge);
-
-        neighbors[sourceNode].add(forwardEdge);
-        neighbors[destinationNode].add(backwardEdge);
-    }
-
-    public List<Edge>[] getNeighbors() {
-        return neighbors;
+        forwardEdge.setReverseEdge(backwardEdge);
+        backwardEdge.setReverseEdge(forwardEdge);
+        adjacencyList[sourceNode].add(forwardEdge);
+        adjacencyList[destinationNode].add(backwardEdge);
     }
 
     public int getNumberOfNodes() {
         return numberOfNodes;
+    }
+
+    public List<Edge>[] getAdjacencyList() {
+        return adjacencyList;
     }
 }
