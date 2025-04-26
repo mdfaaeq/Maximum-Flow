147cbf30288794007e05c9c8b0cf7e55580f845f
Faaeq Fazal <faaeqavl@gmail.com>
2025-04-26 21:22:41 +0530

    Added a bit of comments to understand the code properly

--- NetworkFlow/src/graph/Graph.java
+++ NetworkFlow/src/graph/Graph.java
@@ -3,31 +3,48 @@ package graph;
 import java.util.*;
 
 public class Graph {
+    // Total number of nodes in the graph
     private int numberOfNodes;
+    // Adjacency list to store edges for each node
     private List<Edge>[] adjacencyList;
 
     @SuppressWarnings("unchecked")
     public Graph(int numberOfNodes) {
         this.numberOfNodes = numberOfNodes;
+        // Initialized the adjacency list for each node
         adjacencyList = new ArrayList[numberOfNodes];
         for (int i = 0; i < numberOfNodes; i++) {
             adjacencyList[i] = new ArrayList<>();
         }
     }
 
+    /*
+     * Adds a forward edge and a corresponding backward (reverse) edge between two nodes
+     * Forward edge has the given capacity
+     * Backward edge initially has 0 capacity (used for residual graph in flow algorithms)
+     */
     public void addEdge(int sourceNode, int destinationNode, int capacity) {
+        // Created the forward edge with the given capacity
         Edge forwardEdge = new Edge(sourceNode, destinationNode, capacity);
+
+        // Created the backward edge with 0 capacity
         Edge backwardEdge = new Edge(destinationNode, sourceNode, 0);
+
+        // Link the forward and backward edges to each other
         forwardEdge.setReverseEdge(backwardEdge);
         backwardEdge.setReverseEdge(forwardEdge);
+
+        // Add the edges to the adjacency list
         adjacencyList[sourceNode].add(forwardEdge);
         adjacencyList[destinationNode].add(backwardEdge);
     }
 
+    // Returns the number of nodes in the graph
     public int getNumberOfNodes() {
         return numberOfNodes;
     }
 
+    // Returns the adjacency list representing the graph
     public List<Edge>[] getAdjacencyList() {
         return adjacencyList;
     }
